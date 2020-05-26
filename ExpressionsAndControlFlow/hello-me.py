5a53e6798d3ddf7451fe6b128bb4c75d1b05fe89
Robi <robpowwwa@gmail.com>
2020-05-26 10:47:20 +0200

    hello-me.py added

---
+++ ExpressionsAndControlFlow/hello-me.py
@@ -0,0 +1,8 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\Robi\Documents\Python\PythonLearn\ExpressionsAndControlFlow\hello-me.py
+Hello, me!
+>>> print(' elbasztam a teaching materials eredeti hello me filet, nem emlékszem ,hogy csak ennyi volt e feladat ,hogyprinteljek :DDD')
+ elbasztam a teaching materials eredeti hello me filet, nem emlékszem ,hogy csak ennyi volt e feladat ,hogyprinteljek :DDD
+>>> 
