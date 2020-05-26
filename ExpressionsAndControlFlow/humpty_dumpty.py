6651026237b7251669ebf167ffb6340b6056d93d
Robi <robpowwwa@gmail.com>
2020-05-26 12:16:37 +0200

    added humpty dumpty

---
+++ ExpressionsAndControlFlow/humpty_dumpty.py
@@ -0,0 +1,7 @@
+# Modify this program to print Humpty Dumpty riddle correctly
+print("Humpty Dumpty sat on a wall,")
+print("Humpty Dumpty had a great fall.")
+print("All the king's horses and all the king's men")
+print("Couldn't put Humpty together again.")
+
+print( " Modified by changing by changing the strings, is there any command to change the order of the lines?")
