25e434cd918379c2b353be603892571c806fd425
Robi <robpowwwa@gmail.com>
2020-05-26 16:55:48 +0200

    added bmi, fav no, swap

---
+++ ExpressionsAndControlFlow/bmi.py
@@ -0,0 +1,12 @@
+massInKg = 81.2
+heightInM = 1.78
+
+#Print the Body mass index (BMI) based on these values
+
+massKG= 68.4
+heightM=1.71
+
+BMI= massKG / heightM ** 2
+print(BMI)
+
+# Táblázat szerint teljesen normális vagyok 
---
+++ ExpressionsAndControlFlow/favorite_number.py
@@ -0,0 +1,7 @@
+favorite_number = 8
+# Store your favorite number in a variable (as a number)
+# And print it like this: "My favorite number is: 8"
+
+x=13
+print('my favorite number is:', x)
+#erre gondol a feladat? :D
