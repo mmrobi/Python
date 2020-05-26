e4c257badd4347fa629cf4314c50499b1593d752
Robi <robpowwwa@gmail.com>
2020-05-26 12:38:30 +0200

    added some exercises

---
+++ ExpressionsAndControlFlow/hello_others.py
@@ -0,0 +1,11 @@
+# Greet 3 of your classmates with this program, in three separate lines
+# like:
+#
+# Hello, Esther!
+# Hello, Mary!
+# Hello, Joe!
+
+
+print('Hello, Joe!')
+print('Hello, Johnyka!')
+print('Hello, Joeci!')
