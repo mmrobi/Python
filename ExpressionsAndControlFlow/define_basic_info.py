66341b13bd4c9741c19e567b5c935901920cd267
Robi <robpowwwa@gmail.com>
2020-05-26 17:06:22 +0200

    added basic info

---
+++ ExpressionsAndControlFlow/define_basic_info.py
@@ -0,0 +1,17 @@
+# Define several things as a variable then print their values
+# Your name as a string
+# Your age as an integer
+# Your height in meters as a float
+# Whether you are married or not as a boolean
+
+age= 25
+heightM= 1.71
+married = False
+
+print('Marzea-Mezei Róbert')
+
+print('my age:', age)
+
+print('my height:', heightM)
+
+print('Married: ', married)
