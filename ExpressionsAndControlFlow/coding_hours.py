8b0b2ca9623ecfb3196bd44ea5d5bc84af30bc3c
Robi <robpowwwa@gmail.com>
2020-05-26 12:58:51 +0200

    added coding hours.py

---
+++ ExpressionsAndControlFlow/coding_hours.py
@@ -0,0 +1,23 @@
+# An average Green Fox attendee codes 6 hours daily
+# The semester is 17 weeks long
+#
+# Print how many hours is spent with coding in a semester by an attendee,
+# if the attendee only codes on workdays.
+#
+# Print the percentage of the coding hours in the semester if the average
+# work hours weekly is 52
+
+print('In 17 weeks there are:' + str(17*5) + ' days')
+
+print('attendees spend coding:' + str(85*6) + ' hours in a semester')
+
+print(str(5*6) + ' hours spent coding weekly')
+
+print((5*6*17)/(17*52)*100)
+
+print('57,7% of the weekly working hours are spent coding')
+
+
+
+
+#jó rég matekoztam, fun ;) 
