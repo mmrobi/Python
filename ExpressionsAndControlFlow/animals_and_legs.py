f5b56c2c87fac2e8787be2c6342946e36a0e4286
Robi <robpowwwa@gmail.com>
2020-05-27 14:11:02 +0200

    animals and legs, fun volt

---
+++ ExpressionsAndControlFlow/animals_and_legs.py
@@ -0,0 +1,13 @@
+# Write a program that asks for two integers
+# The first represents the number of chickens the farmer has
+# The second represents the number of pigs owned by the farmer
+# It should print how many legs all the animals have
+
+
+a=int(input('How many chickens does the farmer have? '))
+b=a*2
+c=int(input('How many pigs does the farmer own?' ))
+d=c*4
+print('Sum of the animal legs: ', b+d)
+
+
