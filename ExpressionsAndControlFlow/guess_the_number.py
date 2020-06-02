2adfd1b5f25bb88ae0929514b72590b01cd96ef3
Robi <robpowwwa@gmail.com>
2020-06-02 18:15:29 +0200

    a draw diagonal nem az, aminek kéne lennie, guess the number kész

---
+++ ExpressionsAndControlFlow/guess_the_number.py
@@ -0,0 +1,20 @@
+# Write a program that stores a number, and the user has to figure it out.
+# The user can input guesses, after each guess the program would tell one
+# of the following:
+#
+# The stored number is higher
+# The stried number is lower
+# You found the number: 8
+
+Num=13
+x=int(input("Guess the number: "))
+
+while x>Num:
+    print("The stored number is lower  \n")
+    x=int(input("Guess the number: "))
+    while x<Num:
+        print("The stored number is higher \n")
+        x=int(input("Guess the number: "))
+if x==Num:
+    print("You found the number: " , Num)
+
