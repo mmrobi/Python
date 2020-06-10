804c7baca89ed862f597c05c9fc43471d1e5b8a9
Robi <robpowwwa@gmail.com>
2020-06-10 12:12:24 +0200

    started functions

---
+++ Functions/greet.py
@@ -0,0 +1,11 @@
+# - Create variable named `al` and assign the value `Green Fox` to it
+# - Create a function called `greet` that greets it's input parameter
+#     - Greeting is printing e.g. `Greetings, dear Green Fox`
+# - Greet `al`
+
+al= 'Green fox'
+
+def greet(name):
+    print("Greetings ,dear", name)
+
+greet(al)
---
+++ Functions/sum.py
@@ -0,0 +1,12 @@
+# Write a function called `sum` that returns the sum of numbers from zero to the given parameter
+
+
+def Sum(x):
+    for x in range(0,Sum):
+        print(sum(x))
+
+Sum(10)
+    
+
+
+
